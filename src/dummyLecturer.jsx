e98aa27228750965fff960d82107257578fa0206
KhanyiMth <khanyisilekelebogile@gmail.com>
2025-04-24 00:59:55 +0200

    Add new dummy data file

---
+++ src/dummyLecturer.jsx
@@ -0,0 +1,12 @@
+const dummyLecturer = {
+  id: 1,
+  name: "John",
+  surname: "Doe",
+  phone: "+27 82 123 4567",
+  email: "john.doe@campus.edu",
+  department: "Computer Science",
+  oldPassword: "OldPassword123!",
+  newPassword: ""
+};
+
+export default dummyLecturer;
